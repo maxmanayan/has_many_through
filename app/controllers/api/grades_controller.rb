9dc363a3a74d61e65e021e14a533b8d7a0f1df96
Max Manayan <maximillian521@gmail.com>
2021-03-18 11:01:51 -0600

    models and controllers created

---
+++ app/controllers/api/grades_controller.rb
@@ -0,0 +1,43 @@
+class Api::GradesController < ApplicationController
+  before_action :set_grade, only: [:show, :destroy]
+  
+  def index
+    grades = Grade.all 
+
+    render json: grades
+  end
+
+
+  def show 
+    render json: @grade
+  end
+
+
+  def create
+    grade = Grade.new(grade_params)
+
+    if(grade.save)
+      render json: grade 
+    else
+      render json: {error: @grade.errors}, status: 422
+    end
+  end
+
+  
+  def destroy
+   @grade.destroy
+   render json: @grade
+  end
+
+
+  
+  private 
+
+  def set_grade 
+    @grade = Grade.find(params[:id])
+  end
+
+  def grade_params
+    params.require(:grade).permit(:score, :user_id, :skill_id)
+  end
+end
